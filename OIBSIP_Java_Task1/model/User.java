fd732931add9364523c87dc086228df982e88774
mubashirhussain482 <malikmubashirhussain482@gmail.com>
2026-08-11 16:54:20 +0500

    Add User class with username and password fields

---
+++ OIBSIP_Java_Task1/model/User.java
@@ -0,0 +1,24 @@
+package OIBSIP_Java_Task1.model;
+public class User {
+    private String userName;
+    private String password;
+    public User() {
+    }
+    public User(String userName,String password){
+        this.userName=userName;
+        this.password=password;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setUserName(String userName){
+        this.userName=userName;
+        
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+}
